52654c2669fb283251c416dc2d7f04283d5e4f5d
Houcemeddine Turki <turkiabdelwaheb@hotmail.fr>
2026-08-13 11:20:56 +0100

    Change base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/SfaxHeritage/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
